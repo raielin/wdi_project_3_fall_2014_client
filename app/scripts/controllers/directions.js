b51128a6dbc3862890ab359b31b23cbfd0c003e3
raielin@gmail.com <raielin@gmail.com>
2014-12-15 17:38:27 -0500

    Ran $ yo angular:controller directions. Grouped script links together in index.html.

---
+++ app/scripts/controllers/directions.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stopInApp.controller:DirectionsCtrl
+ * @description
+ * # DirectionsCtrl
+ * Controller of the stopInApp
+ */
+angular.module('stopInApp')
+  .controller('DirectionsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
