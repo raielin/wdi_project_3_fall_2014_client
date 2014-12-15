a1998b3462f4c5ff4b2646d4d28851e77f09b530
raielin@gmail.com <raielin@gmail.com>
2014-12-15 17:40:57 -0500

    Ran yo generator for places controller and both directions and places factories.

---
+++ app/scripts/services/directions.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name stopInApp.directions
+ * @description
+ * # directions
+ * Service in the stopInApp.
+ */
+angular.module('stopInApp')
+  .service('directions', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
