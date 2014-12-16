235501c1658969b9f3d21aedf9fb12130bd55b8d
raielin@gmail.com <raielin@gmail.com>
2014-12-16 10:12:22 -0500

    Added back in comments from Yeoman generator.

--- app/scripts/services/directions.js
+++ app/scripts/services/directions.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/**
+ * @ngdoc service
+ * @name stopInApp.DirectionsFactory
+ * @description
+ * # DirectionsFactory
+ * Factory in the stopInApp.
+ */
+
 angular.module('stopInApp').factory('DirectionsFactory', function($http, MapsAPI) {
 
   var directions = [];
