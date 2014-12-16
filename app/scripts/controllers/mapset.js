c958a87fb15ebe658ac54c835281c6c72656a2f9
raielin@gmail.com <raielin@gmail.com>
2014-12-15 22:22:53 -0500

    Ran $ yo angular:controller mapSet

---
+++ app/scripts/controllers/mapset.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stopInApp.controller:MapsetCtrl
+ * @description
+ * # MapsetCtrl
+ * Controller of the stopInApp
+ */
+angular.module('stopInApp')
+  .controller('MapsetCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
