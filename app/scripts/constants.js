5fdd6840e2823308795a65fac56fa5b096e507b4
raielin@gmail.com <raielin@gmail.com>
2014-12-17 10:10:25 -0500

    Deleted unneeded code

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -1,5 +1,3 @@
 angular.module('stopInApp')
   // _ sets lo-dash underscore as a known element
-  .constant('_', window._)
-  .constant('MapsAPI', 'https://maps.googleapis.com/maps/api/js?v=3.exp')
-  .constant('PlacesLibrary', 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places');
+  .constant('_', window._);
